c1624f0bbf8e44c7f1086d1f5a83d0f40f93380e
Matt Linscott <744765+kelnei@users.noreply.github.com>
2026-03-20 22:18:42 -0700

    add fzf install script and bash integration

---
+++ bash/.bashrc.d/50-fzf.sh
@@ -0,0 +1,34 @@
+# fzf
+export FZF_DEFAULT_OPTS='--height 40% --layout reverse --border'
+export FZF_CTRL_T_OPTS='--walker-skip .git,node_modules,target --bind ctrl-/:change-preview-window(down|hidden|)'
+export FZF_ALT_C_OPTS='--walker-skip .git,node_modules,target'
+
+if command -v batcat &>/dev/null; then
+    export FZF_CTRL_T_OPTS="$FZF_CTRL_T_OPTS --preview '\''batcat -n --color=always {}'\''"
+    export FZF_FILE_PREVIEW='batcat -n --color=always'
+elif command -v bat &>/dev/null; then
+    export FZF_CTRL_T_OPTS="$FZF_CTRL_T_OPTS --preview '\''bat -n --color=always {}'\''"
+    export FZF_FILE_PREVIEW='bat -n --color=always'
+else
+    export FZF_FILE_PREVIEW='sed -n 1,200p'
+fi
+
+if command -v fzf &>/dev/null; then
+    eval "$(fzf --bash)"
+fi
+
+fe() {
+    FILE=$(rg --files --hidden -g '!.git' 2>/dev/null | fzf --preview "$FZF_FILE_PREVIEW {}")
+
+    if [ -n "$FILE" ]; then
+        nvim "$FILE"
+    fi
+}
+
+fbr() {
+    BRANCH=$(git for-each-ref --format='%(refname:short)' refs/heads refs/remotes 2>/dev/null | fzf)
+
+    if [ -n "$BRANCH" ]; then
+        git checkout "$BRANCH"
+    fi
+}
