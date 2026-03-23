d7dce03fc81ecf8159dd4e3456b0034ede84f17b
Matt Linscott <744765+kelnei@users.noreply.github.com>
2026-03-23 12:43:43 -0700

    add .bashrc.secrets.d for machine-local secret exports

--- bash/.bashrc.d/10-dirs.sh
+++ bash/.bashrc.d/10-dirs.sh
@@ -1,2 +1,3 @@
 # Ensure standard project directories exist
 mkdir -p "$HOME/code"
+mkdir -p "$HOME/.bashrc.secrets.d"
---
+++ bash/.bashrc.d/15-secrets.sh
@@ -0,0 +1,7 @@
+# Source secret environment variables (not tracked by git)
+if [ -d "$HOME/.bashrc.secrets.d" ]; then
+    for f in "$HOME/.bashrc.secrets.d"/*.sh; do
+        [ -r "$f" ] && source "$f"
+    done
+    unset f
+fi
