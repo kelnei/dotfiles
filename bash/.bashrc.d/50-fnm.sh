f5b6ca3c197ae6241e9e612879839694da8a61f0
Matt Linscott <744765+kelnei@users.noreply.github.com>
2026-04-18 09:53:38 -0700

    make fnm use ~/.local/state for multishells instead of XDG_RUNTIME_DIR

--- bash/.bashrc.d/50-fnm.sh
+++ bash/.bashrc.d/50-fnm.sh
@@ -1,2 +1,8 @@
 # fnm - Fast Node Manager
+# make fnm fallback to ~/.local/state/fnm_multishells
+# instead of /run/user/1000/fnm_multishells
+_old_xdg="$XDG_RUNTIME_DIR"
+unset XDG_RUNTIME_DIR
 eval "$(fnm env --use-on-cd --shell bash)"
+[ -n "$_old_xdg" ] && export XDG_RUNTIME_DIR="$_old_xdg"
+unset _old_xdg
