0cbbc5cb6515fad283be2b53d7c2569b35a3a3ef
Matt Linscott <744765+kelnei@users.noreply.github.com>
2026-04-29 19:50:13 -0700

    fix: use /tmp for fnm_multishells so codex doesn't throw symlink errors

--- bash/.bashrc.d/50-fnm.sh
+++ bash/.bashrc.d/50-fnm.sh
@@ -1,8 +1,8 @@
 # fnm - Fast Node Manager
-# make fnm fallback to ~/.local/state/fnm_multishells
+# make fnm fallback to /tmp/fnm_multishells
 # instead of /run/user/1000/fnm_multishells
 _old_xdg="$XDG_RUNTIME_DIR"
-unset XDG_RUNTIME_DIR
+XDG_RUNTIME_DIR="/tmp"
 eval "$(fnm env --use-on-cd --shell bash)"
 [ -n "$_old_xdg" ] && export XDG_RUNTIME_DIR="$_old_xdg"
 unset _old_xdg
